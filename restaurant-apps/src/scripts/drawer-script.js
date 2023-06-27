c5d9ec45bee1d16ed21742f6aef11bd132fff69d
luquilo <luquilo.etc@gmail.com>
2023-06-28 00:42:07 +0700

    done

--- restaurant-apps/src/scripts/drawer-script.js
+++ restaurant-apps/src/scripts/drawer-script.js
@@ -1,25 +1,25 @@
 // make the drawer appear
-const drawerElement = document.querySelector('#drawer');
-const hamburgerButtonElement = document.querySelector('#hamburger');
-const mainElement = document.querySelector('main');
+const drawerElement = document.querySelector("#drawer");
+const hamburgerButtonElement = document.querySelector("#hamburger");
+const mainElement = document.querySelector("main");
 
 // make the drawer appearence
-hamburgerButtonElement.addEventListener('click', event => {
-    drawerElement.classList.toggle('open');
-    event.stopPropagation();
-})
+hamburgerButtonElement.addEventListener("click", (event) => {
+  drawerElement.classList.toggle("open");
+  event.stopPropagation();
+});
 
 // make the drawer disappear
-mainElement.addEventListener('click', event => {
-    drawerElement.classList.toggle('close');
-    drawerElement.classList.remove('open');
-    event.stopPropagation();
-})
+mainElement.addEventListener("click", (event) => {
+  drawerElement.classList.toggle("close");
+  drawerElement.classList.remove("open");
+  event.stopPropagation();
+});
 
 // membuat fungsi sementara untuk menerapkan style ease-out css pada drawer
-mainElement.addEventListener('click', event => {
-    setTimeout(() => {
-        drawerElement.classList.remove('close');
-        event.stopPropagation()
-    }, 500);
-})
+mainElement.addEventListener("click", (event) => {
+  setTimeout(() => {
+    drawerElement.classList.remove("close");
+    event.stopPropagation();
+  }, 500);
+});
