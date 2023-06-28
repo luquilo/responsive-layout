49cae2d7bf01e5fd1e365a1ebdc44a7098860c99
luquilo <luquilo.etc@gmail.com>
2023-06-28 07:43:25 +0700

    debugging webpack stuff

--- restaurant-apps/webpack.prod.js
+++ restaurant-apps/webpack.prod.js
@@ -1,5 +1,6 @@
 const common = require('./webpack.common');
 const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -18,6 +19,20 @@ module.exports = merge(common, {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+        ],
+      },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
 });
