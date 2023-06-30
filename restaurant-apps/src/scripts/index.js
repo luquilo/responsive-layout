c2f20bf4d9793b3f18c23e4f931ddc1fd0d5042f
luquilo <luquilo.etc@gmail.com>
2023-06-30 22:28:59 +0700

    debungging webpack image

--- restaurant-apps/src/scripts/index.js
+++ restaurant-apps/src/scripts/index.js
@@ -1,9 +1,16 @@
 import 'regenerator-runtime'; /* for async await transpile */
+
+// import css
 import '../styles/main.css';
 import '../styles/small.css';
 import '../styles/medium.css';
 import '../styles/large.css';
 import '../styles/x-large.css';
+
+
+// import gambar
+// import
+
 
 
 import '../scripts/drawer-script.js';
--- restaurant-apps/webpack.common.js
+++ restaurant-apps/webpack.common.js
@@ -41,8 +41,8 @@ module.exports = {
             loader: "file-loader",
             options: {
               name: "[name].[ext]",
-              outputPath: "images/",
-              publicPath: "public/images/",
+              outputPath: "images",
+              publicPath: "images",
             },
           },
         ],
