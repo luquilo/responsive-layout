892ac280cf35d4a729a0641c0135e4d3dbcc480d
luquilo <luquilo.etc@gmail.com>
2023-07-07 20:04:23 +0700

    js clean code finished

---
+++ 5-js clean code/eslint-sample/src/object.js
@@ -0,0 +1,16 @@
+// object with good data structure
+
+// getter and setter
+
+// class method chaining { https://www.dicoding.com/academies/219/tutorials/9371?from=9366 }
+
+// solid
+
+// open closed principle (OCP), menambah fitur pada branch utama, tapi tidak merubah fitur yang ada
+// { https://www.dicoding.com/academies/219/tutorials/9376?from=9371 }
+
+// liskov subtitution principle
+
+// interface segregation principle
+
+// Dependencies Inversion Principle (DIP)
